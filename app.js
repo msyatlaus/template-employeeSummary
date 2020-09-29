0cf688052731cefe51606728ad6acd2370697e82
Bradley Guidry <bradleyguidry@gmail.com>
2020-09-28 20:59:39 -0500

    adds employee input function

--- app.js
+++ app.js
@@ -9,8 +9,29 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const team = [];
 
 
+function employeeInformation() {
+    inquirer.prompt([
+     {
+       type: "list",
+       message: "What type of employee would you like to input",
+       name: "name",
+       choices: ["Manager", "Engineer", "Intern", "Show Summary"],
+     },
+   ]).then(val => {
+     if (val.name === "Manager") {
+       managerInformation();
+     } else if (val.name === "Engineer") {
+       engineerInformation()
+     } else if (val.name === "Intern") {
+       internInformation();
+     } else if (val.name === "Show Summary") {
+       generateHTML(outputPath, render(team));
+     };
+   }); 
+ }; 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
